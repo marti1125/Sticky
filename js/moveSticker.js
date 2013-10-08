ecdd7c4d0d1704f73d417f8d88fff56dd4754d0e
Aaron Druck <aldruck@gmail.com>
2013-10-08 14:15:15 -0700

    got the drag to work
    but it erases the bg

--- js/moveSticker.js
+++ js/moveSticker.js
@@ -5,55 +5,112 @@ $(".sticker-1").click(function(){
     //add object element to canvas
     var img = new Image();
     img.onload = function(){
-        ctx.drawImage(img, 0,0);
+        context.drawImage(img, 0,0);
     };
     img.src = "http://images.christmastimeclipart.com/images/2/1271716593176_1788/img_1271716593176_17881.jpg";
 
     var canvas=document.getElementById("myCanvas");
-    var ctx=canvas.getContext("2d");
+    var context=canvas.getContext("2d");
     var canvasOffset=$("#myCanvas").offset();
     var offsetX=canvasOffset.left;
     var offsetY=canvasOffset.top;
     var canvasWidth=canvas.width;
     var canvasHeight=canvas.height;
-    var isDragging=false;
 
-    function handleMouseDown(e){
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-      // set the drag flag
-      isDragging=true;
+    // create a drawer which tracks touch movements
+    var drawer = {
+        isDragging: false,
+        touchstart: function (coors) {
+            context.beginPath();
+            context.moveTo(coors.x, coors.y);
+            this.isDragging = true;
+        },
+        touchmove: function (coors) {
+            if (this.isDragging) {
+                //context.lineTo(coors.x, coors.y);
+                //context.stroke();
+
+                context.clearRect(0,0,canvasWidth,canvasHeight);
+                // image, x, y, w, h
+                context.drawImage(img,coors.x-120/2,coors.y-120/2,120,120);
+            }
+        },
+        touchend: function (coors) {
+            if (this.isDragging) {
+                this.touchmove(coors);
+                this.isDrawing = false;
+            }
+        }
+    };
+
+
+
+    // create a function to pass touch events and coordinates to drawer
+    function draw(event) {
+        var type = null;
+        // map mouse events to touch events
+        switch(event.type){
+            case "mousedown":
+                    event.touches = [];
+                    event.touches[0] = {
+                        pageX: event.pageX,
+                        pageY: event.pageY
+                    };
+                    type = "touchstart";
+            break;
+            case "mousemove":
+                    event.touches = [];
+                    event.touches[0] = {
+                        pageX: event.pageX,
+                        pageY: event.pageY
+                    };
+                    type = "touchmove";
+            break;
+            case "mouseup":
+                    event.touches = [];
+                    event.touches[0] = {
+                        pageX: event.pageX,
+                        pageY: event.pageY
+                    };
+                    type = "touchend";
+            break;
+        }
+
+        // touchend clear the touches[0], so we need to use changedTouches[0]
+        var coors;
+        if(event.type === "touchend") {
+            coors = {
+                x: event.changedTouches[0].pageX,
+                y: event.changedTouches[0].pageY
+            };
+        }
+        else {
+            // get the touch coordinates
+            coors = {
+                x: event.touches[0].pageX,
+                y: event.touches[0].pageY
+            };
+        }
+        type = type || event.type
+        // pass the coordinates to the appropriate handler
+        drawer[type](coors);
     }
 
-    function handleMouseUp(e){
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-      // clear the drag flag
-      isDragging=false;
-    }
+    // detect touch capabilities
+    var touchAvailable = ('createTouch' in document) || ('ontouchstart' in window);
 
-    function handleMouseOut(e){
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-      // user has left the canvas, so clear the drag flag
-      isDragging=false;
+    // attach the touchstart, touchmove, touchend event listeners.
+    if(touchAvailable){
+        canvas.addEventListener('touchstart', draw, false);
+        canvas.addEventListener('touchmove', draw, false);
+        canvas.addEventListener('touchend', draw, false);
     }
-
-    function handleMouseMove(e){
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-      // if the drag flag is set, clear the canvas and draw the image
-      if(isDragging){
-          ctx.clearRect(0,0,canvasWidth,canvasHeight);
-          // image, x, y, w, h
-          ctx.drawImage(img,canMouseX-120/2,canMouseY-120/2,120,120);
-      }
+    // attach the mousedown, mousemove, mouseup event listeners.
+    else {
+        canvas.addEventListener('mousedown', draw, false);
+        canvas.addEventListener('mousemove', draw, false);
+        canvas.addEventListener('mouseup', draw, false);
     }
-
-    $("#myCanvas").mousedown(function(e){handleMouseDown(e);});
-    $("#myCanvas").mousemove(function(e){handleMouseMove(e);});
-    $("#myCanvas").mouseup(function(e){handleMouseUp(e);});
-    $("#myCanvas").mouseout(function(e){handleMouseOut(e);});
 
     // prevent elastic scrolling
     document.body.addEventListener('touchmove',function(event){
