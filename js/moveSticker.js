39eb6f5f8223d50a9d2c8ff3b4afe60d60308eaa
Aaron Druck <aldruck@gmail.com>
2013-10-17 19:45:55 -0700

    updating the stickers

--- js/moveSticker.js
+++ js/moveSticker.js
@@ -10,6 +10,21 @@
 //var can3 = document.getElementById('canvas3');
 //var ctx3 = can3.getContext('2d');
 
+$(".btn-place").click(function(){
+    $(".topbarslidedown").animate({
+      opacity: 0
+    }, 1000);
+});
+
+$(".btn-remove").click(function(){
+    $(".topbarslidedown").animate({
+      opacity: 0
+    }, 1000);
+});
+
+
+
+
 $(".sticker-1").click(function(){
     //alert("add sticker");
     //move down the modal
@@ -17,11 +32,73 @@ $(".sticker-1").click(function(){
       top: 1000
     }, 200);
 
+    $("#newimage").attr("src","images/stickers/fox-mask.png");
+
     $("#newimage").show();
-    $sticker.show();
+    //$sticker.show();
+
+    // show tool bar with Done/Checkmark and Undo/Delete at the top
+    $(".topbarslidedown").animate({
+      opacity: 1
+    }, 1000);
+
+
+
     //$('#resizable').show();
     //$( "#sticker" ).resizable();
 
 
 });
+
+$(".sticker-2").click(function(){
+    $("#stickers-screen").animate({
+      top: 1000
+    }, 200);
+    $("#newimage").attr("src","images/stickers/sunglasses.png");
+    $("#newimage").show();
+    //$sticker.show();
+
+});
+
+$(".sticker-3").click(function(){
+    $("#stickers-screen").animate({
+      top: 1000
+    }, 200);
+    $("#newimage").attr("src","images/stickers/mustache.png");
+    $("#newimage").show();
+    //$sticker.show();
+
+});
+
+$(".sticker-4").click(function(){
+    $("#stickers-screen").animate({
+      top: 1000
+    }, 200);
+    $("#newimage").attr("src","images/stickers/hat.png");
+    $("#newimage").show();
+    //$sticker.show();
+
+});
+
+$(".sticker-5").click(function(){
+    $("#stickers-screen").animate({
+      top: 1000
+    }, 200);
+    $("#newimage").attr("src","images/stickers/nose.png");
+    $("#newimage").show();
+    //$sticker.show();
+
+});
+
+$(".sticker-6").click(function(){
+    $("#stickers-screen").animate({
+      top: 1000
+    }, 200);
+    $("#newimage").attr("src","images/stickers/tail.png");
+    $("#newimage").show();
+    //$sticker.show();
+
+});
+
+
 
